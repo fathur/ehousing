b7cfca5ad36220058151559b3d9e8d8a1e2a8802
Fathur Rohman <fathur_rohman17@yahoo.co.id>
2016-09-10 13:17:33 +0700

    dashboard nasional padding logo

--- app/views/front/nasional/dashboard.blade.php
+++ app/views/front/nasional/dashboard.blade.php
@@ -16,19 +16,83 @@
     <div class="row row-offcanvas row-offcanvas-right">
         <div class="col-xs-12 col-md-9">
 
-            <div class="jumbotron">
-                <div class="col-sm-3 col-xs-12">
+            <div class="jumbotron pu-nasional-logo">
+                <div class="logo">
                     <img src="{{asset('img/logo-pupr-300.jpg')}}" alt="PUPR" style="height: 100px; height: 100px;">
                 </div>
-                <div class="col-sm-9 col-xs-12">
+                <div class="text">
                     <h1>e-Housing</h1>
-                    <p>Ditjen Penyediaan Perumahan<br/>
+                    <p>Direktorat Jendral Penyediaan Perumahan<br/>
                         Kementerian Pekerjaan Umum dan Perumahan Rakyat</p>
                 </div>
 
                 <div style="clear: both"></div>
 
             </div>
+
+            <div class="row">
+                <div class="col-xs-7">
+                    <div class="ibox float-e-margins">
+                        <div class="ibox-title">
+                            <h5>Animation and Caption</h5>
+                            <div class="ibox-tools">
+                                <a class="collapse-link">
+                                    <i class="fa fa-chevron-up"></i>
+                                </a>
+                                <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                                    <i class="fa fa-wrench"></i>
+                                </a>
+                                <ul class="dropdown-menu dropdown-user">
+                                    <li><a href="#">Config option 1</a>
+                                    </li>
+                                    <li><a href="#">Config option 2</a>
+                                    </li>
+                                </ul>
+                                <a class="close-link">
+                                    <i class="fa fa-times"></i>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="ibox-content " style="display: block;">
+                            <div class="carousel slide" id="carousel2">
+                                <ol class="carousel-indicators">
+                                    <li data-slide-to="0" data-target="#carousel2" class="active"></li>
+                                    <li data-slide-to="1" data-target="#carousel2"></li>
+                                    <li data-slide-to="2" data-target="#carousel2" class=""></li>
+                                </ol>
+                                <div class="carousel-inner">
+                                    <div class="item active">
+                                        <img alt="image" class="img-responsive" src="http://webapplayers.com/inspinia_admin-v2.6/img/p_big1.jpg">
+                                        <div class="carousel-caption">
+                                            <p>This is simple caption 1</p>
+                                        </div>
+                                    </div>
+                                    <div class="item ">
+                                        <img alt="image" class="img-responsive" src="img/p_big3.jpg">
+                                        <div class="carousel-caption">
+                                            <p>This is simple caption 2</p>
+                                        </div>
+                                    </div>
+                                    <div class="item">
+                                        <img alt="image" class="img-responsive" src="img/p_big2.jpg">
+                                        <div class="carousel-caption">
+                                            <p>This is simple caption 3</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <a data-slide="prev" href="#carousel2" class="left carousel-control">
+                                    <span class="icon-prev"></span>
+                                </a>
+                                <a data-slide="next" href="#carousel2" class="right carousel-control">
+                                    <span class="icon-next"></span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
 
             <div class="row">
                 <div class="col-lg-12">
