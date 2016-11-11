43ad65ec801c50af98126545646d2ade53e2bd26
Fathur Rohman <fathur_rohman17@yahoo.co.id>
2016-11-11 07:54:01 +0700

    fix dashboard error

--- app/views/front/nasional/dashboard.blade.php
+++ app/views/front/nasional/dashboard.blade.php
@@ -249,7 +249,7 @@
                                     <div class="col-sm-6 col-md-4">
                                         <a href="{{{ $feed->get_link() }}}" class="btn-link" target="_blank"><h3>{{{ $feed->get_title() }}}</h3></a>
                                         <div class="small m-b-xs">
-                                            <strong>{{{ $feeds->get_channel_tags('','title')[0]['data'] }}}</strong>
+                                            <strong>{{{ strip_tags($feeds->get_channel_tags('','title')[0]['data']) }}}</strong>
                                             <span class="text-muted">
                                                 <i class="fa fa-clock-o"></i> {{{ $feed->get_date() }}}
                                             </span>
@@ -274,23 +274,6 @@
 
         <div class="col-xs-12 col-md-3">
 
-            {{--<div class="list-group">
-                <a href="#" class="list-group-item active"><i class="fa fa-home"></i> Hunian</a>
-
-                @if($hunian->count() > 0)
-                    @foreach($hunian as $item)
-                        --}}{{-- dd($item->toArray()) --}}{{--
-                        <a href="{{route('front.nasional.hunian.show', $item->slug)}}" class='list-group-item'>
-                        <span class='pull-right'>
-                            <small>{{{ $item->Deskripsi }}}</small>
-                        </span> {{{ $item->NamaHunian }}}
-                        </a>
-                    @endforeach
-                @else
-                    <span class="list-group-item">Tidak ditemukan data.</span>
-                @endif
-            </div>
---}}
 
                 <div class="row">
                     <div class="col-lg-12">
@@ -429,94 +412,5 @@
                 @endif
             </div>
 
-
-
-            {{--<div class="list-group">
-                <a href="#" class="list-group-item active">
-                    <i class="fa fa-connectdevelop"></i> Developer
-                </a>
-                @if($developers->count() > 0)
-                    @foreach($developers as $item)
-                        <a href='#' class='list-group-item'>
-                            <span class='pull-right hidden'>
-                                <small>{{{ $item->CreateUid }}}</small>
-                            </span>{{{ $item->Nama }}}</a>
-                    @endforeach
-                @else
-                    <span class="list-group-item">Tidak ditemukan data.</span>
-                @endif
-            </div>
-
-            <div class="list-group">
-                <a href="#" class="list-group-item active">
-                    <i class="fa fa-object-group"></i> Desain &amp; Arsitek
-                </a>
-                @if($arsitek->count() > 0)
-                    @foreach($arsitek as $item)
-                        <a href='#' class='list-group-item'>
-                            <span class='pull-right hidden'>
-                                <small>{{{ $item->CreateUid }}}</small>
-                            </span>{{{ $item->Nama }}}</a>
-                    @endforeach
-                @else
-                    <span class="list-group-item">Tidak ditemukan data.</span>
-                @endif
-            </div>
-            <div class="list-group">
-                <a href="#" class="list-group-item active"><i class="fa fa-cogs"></i> Kontraktor</a>
-                @if($kontraktor->count() > 0)
-                    @foreach($kontraktor as $item)
-                        <a href='#' class='list-group-item'>
-                            <span class='pull-right hidden'>
-                                <small>{{{ $item->CreateUid }}}</small>
-                            </span>{{{ $item->Nama }}}</a>
-                    @endforeach
-                @else
-                    <span class="list-group-item">Tidak ditemukan data.</span>
-                @endif
-            </div>
-            <div class="list-group">
-                <a href="#" class="list-group-item active"><i class="fa fa-user"></i> Tukang</a>
-                @if($tukang->count() > 0)
-                    @foreach($tukang as $item)
-                        <a href='#' class='list-group-item'>
-                            <span class='pull-right hidden'>
-                                <small>{{{ $item->CreateUid }}}</small>
-                            </span>{{{ $item->Nama }}}</a>
-                    @endforeach
-                @else
-                    <span class="list-group-item">Tidak ditemukan data.</span>
-                @endif
-            </div>
-            <div class="list-group">
-                <a href="#" class="list-group-item active"> <i class="fa fa-support"></i> Suplier</a>
-                @if($supplier->count() > 0)
-                    @foreach($supplier as $item)
-                        <a href='#' class='list-group-item'>
-                            <span class='pull-right hidden'>
-                                <small>{{{ $item->CreateUid }}}</small>
-                            </span>{{{ $item->Nama }}}</a>
-                    @endforeach
-                @else
-                    <span class="list-group-item">Tidak ditemukan data.</span>
-                @endif
-            </div>--}}
-            {{--<div class="panel panel-info">
-                <div class="panel-heading">
-                    <a href="#" class="text-white"><i class="fa fa-download"></i> Unduh File</a>
-                </div>
-                <div class="panel-body list-group no-margins no-padding">
-                    @foreach($files as $item)
-                        <a title="{{{ $item->Judul }}}" href="{{route('front.file.download', $item->url)}}" class="list-group-item">
-                        <span class="pull-right">
-                            <small>{{{ $item->downloadcounter }}} <i class="fa fa-download"></i></small>
-                        </span>{{{ Str::limit($item->Judul, 15) }}}.{{{ $item->fileext }}}
-                        </a>
-                    @endforeach
-
-                </div>
-
-            </div>
---}}
         </div>
 @stop
